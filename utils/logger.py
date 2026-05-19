d55f201b7cd34c82a26a4d97ef05333ab7ce5163
rohanxlabs <rohan@gmail.com>
2026-05-19 11:37:54 +0530

    add structured console logger

---
+++ utils/logger.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(
+            logging.Formatter(
+                fmt="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+                datefmt="%H:%M:%S",
+            )
+        )
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
+    return logger
